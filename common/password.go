e3ac51834b9cf6ba03c6cdea6e73032f43e6d099
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-20 17:58:39 +0530

    added functionalities regarding  hashing plain passwd and comparing hashed and plain passwd

---
+++ common/password.go
@@ -0,0 +1,13 @@
+package common
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashedPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
