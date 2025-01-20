ad0514241d910b234fa7482ffaf0f823c9739a54
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-20 17:37:36 +0530

    initialized project

---
+++ cmd/api/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/RhoNit/budgetapi/cmd/api/handlers"
+	"github.com/RhoNit/budgetapi/cmd/api/middlewares"
+	"github.com/RhoNit/budgetapi/cmd/api/routes"
+	"github.com/RhoNit/budgetapi/common"
+	"github.com/RhoNit/budgetapi/internal/mailer"
+	"github.com/RhoNit/budgetapi/internal/migration"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Application struct {
+	logger  echo.Logger
+	server  *echo.Echo
+	handler handlers.Handler
+}
+
+func main() {
+	e := echo.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		e.Logger.Fatal("error loading .env configs")
+	}
+
+	db, err := common.NewPostgres()
+	if err != nil {
+		e.Logger.Fatal(err.Error())
+	}
+
+	migration.MigrateDBUp(db)
+
+	appMailer := mailer.NewMailer(e.Logger)
+
+	h := handlers.Handler{
+		DB:     db,
+		Logger: e.Logger,
+		Mailer: appMailer,
+	}
+
+	app := Application{
+		logger:  e.Logger,
+		server:  e,
+		handler: h,
+	}
+
+	app.server.Use(middleware.Logger())
+	app.server.Use(middlewares.CustomMiddleware)
+
+	routes.Endpoints(app.server, h)
+
+	port := os.Getenv("SERVER_PORT")
+	addr := fmt.Sprintf("127.0.0.1:%s", port)
+	e.Logger.Fatal(e.Start(addr))
+}
