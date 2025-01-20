e422cf2c71bf539fef75d0a0baeb6daea8d4e0ee
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-20 18:11:37 +0530

    single source for all routes i.e. grouped all endpoints

---
+++ cmd/api/routes/endpoints.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/RhoNit/budgetapi/cmd/api/handlers"
+	"github.com/labstack/echo/v4"
+)
+
+func Endpoints(engine *echo.Echo, handler handlers.Handler) {
+	engine.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Budget API's `Hello World` page")
+	})
+	engine.GET("/health", handler.HealthCheck)
+	engine.POST("/register", handler.RegisterUser)
+}
