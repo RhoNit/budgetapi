4b2a400fc58b25383d1862265a7b9ac9d0f5e62c
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-22 00:47:45 +0530

    added seeders logic for category creation

---
+++ internal/migration/seeders/category_seeders.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RhoNit/budgetapi/cmd/api/requests"
+	"github.com/RhoNit/budgetapi/cmd/api/services"
+	"github.com/RhoNit/budgetapi/common"
+)
+
+func main() {
+	db, err := common.NewPostgres()
+	if err != nil {
+		return
+	}
+
+	catSvc := services.NewCategoryService(db)
+
+	categories := []string{
+		"Food", "Fruits", "Gym & Sports", "Skin & Hair Care", "Tech Magazine",
+		"House Rent", "Electricity Bill", "Transportation", "Clothing", "Travel & Vacations",
+		"OTT Subscription", "Emergency Fund", "Insurance", "Investments", "Savings",
+	}
+
+	for _, category := range categories {
+		_, err = catSvc.CreateCategory(&requests.CategoryRequest{
+			Name: category,
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Category: " + category + " created")
+	}
+}
