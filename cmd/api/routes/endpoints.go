fc34c0c196991714782f8c8178ef4077aa754e0a
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-21 19:30:05 +0530

    refactored routes

--- cmd/api/routes/endpoints.go
+++ cmd/api/routes/endpoints.go
@@ -4,14 +4,26 @@ import (
 	"net/http"
 
 	"github.com/RhoNit/budgetapi/cmd/api/handlers"
+	"github.com/RhoNit/budgetapi/cmd/api/middlewares"
 	"github.com/labstack/echo/v4"
 )
 
-func Endpoints(engine *echo.Echo, handler handlers.Handler) {
+func Endpoints(engine *echo.Echo, handler handlers.Handler, mw middlewares.AppMiddleware) {
+	appRoute := engine.Group("/api")
+
+	publicAuthRoutes := appRoute.Group("/auth")
+	{
+		publicAuthRoutes.POST("/register", handler.RegisterUserHandler)
+		publicAuthRoutes.POST("/login", handler.LoginUserHandler)
+	}
+
+	profileRoutes := appRoute.Group("/profile")
+	{
+		profileRoutes.GET("/authenticated-user", handler.GetAuthenticatedUser, mw.AuthMiddleware)
+	}
+
 	engine.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Budget API's `Hello World` page")
 	})
 	engine.GET("/health", handler.HealthCheck)
-	engine.POST("/register", handler.RegisterUserHandler)
-	engine.POST("/login", handler.LoginUserHandler)
 }
