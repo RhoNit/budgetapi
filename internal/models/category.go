4a8fb80c47a31c662a0e39160e83fb0cd92d31c4
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-22 00:38:16 +0530

    added Category model

---
+++ internal/models/category.go
@@ -0,0 +1,11 @@
+package models
+
+type Category struct {
+	Name     string `gorm:"unique;type:varchar(200);not null" json:"name"`
+	Slug     string `gorm:"type:varchar(200);unique;not null" json:"slug"`
+	IsCustom bool   `gorm:"type:bool;default:false" json:"is_custom"`
+}
+
+func (Category) TableName() string {
+	return "categories"
+}
