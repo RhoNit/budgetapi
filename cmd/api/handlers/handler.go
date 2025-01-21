3abc9a06da251b47c5cd57829c27c872d8a7438b
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-22 00:40:59 +0530

    refactored code: added template func to bind request body

--- cmd/api/handlers/handler.go
+++ cmd/api/handlers/handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"errors"
+
 	"github.com/RhoNit/budgetapi/internal/mailer"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -10,4 +12,13 @@ type Handler struct {
 	DB     *gorm.DB
 	Logger echo.Logger
 	Mailer mailer.Mailer
+}
+
+func (h *Handler) BindRequestBody(c echo.Context, request interface{}) error {
+	if err := (&echo.DefaultBinder{}).BindBody(c, request); err != nil {
+		h.Logger.Error(err)
+		return errors.New("failed to bind request body... make sure you are sending a valid payload")
+	}
+
+	return nil
 }
