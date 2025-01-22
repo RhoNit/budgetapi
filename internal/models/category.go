70df1963f17c96ea3750775dd42ca1e18cb9c577
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-22 15:18:01 +0530

    modified Category models a bit

--- internal/models/category.go
+++ internal/models/category.go
@@ -1,6 +1,7 @@
 package models
 
 type Category struct {
+	BaseModel
 	Name     string `gorm:"unique;type:varchar(200);not null" json:"name"`
 	Slug     string `gorm:"type:varchar(200);unique;not null" json:"slug"`
 	IsCustom bool   `gorm:"type:bool;default:false" json:"is_custom"`
