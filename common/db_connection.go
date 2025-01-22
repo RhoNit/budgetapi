2a58f5b919bced4e178590f300443c4c92f1fb37
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-22 15:13:23 +0530

    added TranslateError: true inside gorm.Config

---
+++ common/db_connection.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewPostgres() (*gorm.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		panic("error loading env variables")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, database)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
