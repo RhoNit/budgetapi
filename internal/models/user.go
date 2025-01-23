86791aefe029f02e0c40160c9416069748a6c7f4
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-24 00:42:30 +0530

    added Budgets attribute in User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,10 +4,11 @@ type User struct {
 	BaseModel
 	FirstName      string     `gorm:"type:varchar(200)" json:"first_name"`
 	LastName       string     `gorm:"type:varchar(200)" json:"last_name"`
-	Gender         string     `gorm:"type:varchar(50)" json:"gender"`
+	Gender         *string    `gorm:"type:varchar(50)" json:"gender"`
 	Email          string     `gorm:"type:varchar(100);not null;unique" json:"email"`
 	HashedPassword string     `gorm:"type:varchar(250);not null" json:"-"`
 	Categories     []Category `gorm:"many2many:user_categories;" json:"categories"`
+	Budgets        []Budget   `gorm:"foreignKey:UserID" json:"-"`
 }
 
 func (u *User) TableName() string {
