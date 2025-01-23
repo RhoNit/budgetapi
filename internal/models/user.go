4b79c50c5d2894e090220b7865effa4734189f83
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-23 16:04:36 +0530

    added many2many value as join table in User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,11 +2,12 @@ package models
 
 type User struct {
 	BaseModel
-	FirstName      string `gorm:"type:varchar(200)" json:"first_name"`
-	LastName       string `gorm:"type:varchar(200)" json:"last_name"`
-	Gender         string `gorm:"type:varchar(50)" json:"gender"`
-	Email          string `gorm:"type:varchar(100);not null;unique" json:"email"`
-	HashedPassword string `gorm:"type:varchar(250);not null" json:"-"`
+	FirstName      string     `gorm:"type:varchar(200)" json:"first_name"`
+	LastName       string     `gorm:"type:varchar(200)" json:"last_name"`
+	Gender         string     `gorm:"type:varchar(50)" json:"gender"`
+	Email          string     `gorm:"type:varchar(100);not null;unique" json:"email"`
+	HashedPassword string     `gorm:"type:varchar(250);not null" json:"-"`
+	Categories     []Category `gorm:"many2many:user_categories;" json:"categories"`
 }
 
 func (u *User) TableName() string {
