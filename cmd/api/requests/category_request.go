0f68c948b8b947ca08c6ca17297c7c5268433964
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-23 16:05:45 +0530

    added UserCategoriesAssociationRequest model

--- cmd/api/requests/category_request.go
+++ cmd/api/requests/category_request.go
@@ -4,3 +4,7 @@ type CategoryRequest struct {
 	Name     string `json:"name" validate:"required"`
 	IsCustom bool   `default:"false" json:"is_custom"`
 }
+
+type AssociateUserToCategoriesRequest struct {
+	CategoryIDs []uint `json:"category_ids" validate:"required"`
+}
