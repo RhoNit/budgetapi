2e60483c83d7d10b376e3bcb5c7616ed7708a68f
RhoNit <ranitbiswas.cs@gmail.com>
2025-01-24 00:41:43 +0530

    added a CreatedAt attribute in UserCategory model

--- internal/models/user_category.go
+++ internal/models/user_category.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type UserCategory struct {
 	UserID     uint `gorm:"primaryKey;column:user_id" json:"user_id"`
 	CategoryID uint `gorm:"primaryKey;column:category_id" json:"category_id"`
+	CreatedAt  time.Time
 }
 
 func (UserCategory) TableName() string {
